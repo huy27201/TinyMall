c428edc79cac031d3ed3df1c9db733321dbda4a3
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2021-11-05 01:53:12 +0700

    remove indent

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from '../Header/Header'
 import './Homepage.scss'
 import ProductSlider from '../ProductSlider/ProductSlider'
