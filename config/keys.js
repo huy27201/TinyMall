1a962275455d9621c9f8e9f1259819fa9cf01680
tranmanhduc2711 <manhduc2711@gmail.com>
2021-11-06 17:12:01 +0700

    add upload file api

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI: 'mongodb+srv://dbTinyMall:abc123TDMC@cluster0.tbp0o.mongodb.net/test'
+    mongoURI: 'mongodb+srv://dbTinyMall:abc123TDMC@cluster0.tbp0o.mongodb.net/TinyMall'
 }
