caff4ee0fac0bdf960970cd536220b7ce0a7ebca
Nguyen Gia Huy <nguyengiahuy27201@gmail.com>
2021-11-07 11:31:17 +0700

    adding react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
 import Homepage from './components/Homepage/Homepage'
 import './style/style.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/login">
+            
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App
